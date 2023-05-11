5cd4ce2edc7fedd2c2bb41e08e50f58a1f2c7d92
news <89398483+devahmedehab@users.noreply.github.com>
2023-05-11 17:10:26 +0300

    New

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
-
 import 'package:bloc/bloc.dart';
 import 'package:e_commerce/app/app.dart';
-import 'package:e_commerce/app/constants.dart';
 import 'package:e_commerce/bloc_observer.dart';
 import 'package:e_commerce/data/network/cache_helper.dart';
-import 'package:e_commerce/presentation/layout/shop_layout.dart';
-import 'package:e_commerce/presentation/login/view/login_view.dart';
 import 'package:flutter/material.dart';
-
+import 'app/constants.dart';
 import 'data/network/dio_helper.dart';
-import 'presentation/onBoarding/view/onboarding_view.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +12,7 @@ void main() async{
   DioHelper.init();
   await CacheHelper.init();
  // bool onBoarding =await CacheHelper?.getData(key: 'onBoarding');
-  // token =await CacheHelper.getData(key: 'token');
+   token =await CacheHelper.getData(key: 'token');
  // Widget widget;
 
  /* if (onBoarding !=null ) {
--- lib/presentation/resources/routs_manager.dart
+++ lib/presentation/resources/routs_manager.dart
@@ -1,12 +1,12 @@
 
 import 'package:e_commerce/presentation/layout/shop_layout.dart';
-import 'package:e_commerce/presentation/login/view/login_view.dart';
 import 'package:e_commerce/presentation/onBoarding/view/onboarding_view.dart';
 import 'package:e_commerce/presentation/resources/strings_manager.dart';
 import 'package:e_commerce/presentation/splash/splash_view.dart';
 import 'package:flutter/material.dart';
 
-import '../register/view/register_view.dart';
+import '../auth/login/view/login_view.dart';
+import '../auth/register/view/register_view.dart';
 
 class Routes {
   static const String splashRoute = "/";
